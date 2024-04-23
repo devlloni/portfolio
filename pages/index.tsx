abfa061c1c3a2587e57c2cc74f6c72691c7c378c
Lautaro DelloniÃ <dellonilautaro@gmail.com>
2024-04-23 09:22:20 -0300

    Se remueve el estilo 'jumbo' de fondo por cuestiones de memoria y optimización. En busca de nuevo diseño, corrección de imagen y textos de 'on-development'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 import { HomeSection } from "@/components/Sections/Home";
 import Navbar from "@/components/Navbar/Navbar";
-import { Footer } from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-
-    >
+    <main>
+      <Head>
+        <title>
+          @devlloni | on development
+        </title>
+      </Head>
       <Navbar />
       <div
         className={`min-h-screen ${inter.className}`}
